2de6395988af2d539bc016b2f25840470dbc99ee
Kcee <80968110+irondijonathan@users.noreply.github.com>
2023-07-29 17:27:34 +0000

    build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import htmlPlugin from '@rollup/plugin-html'; // Import the htmlPlugin
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    htmlPlugin({
+      inject: {
+        injectData: {
+          // Add script tags for your animation scripts here
+          scripts: [
+            { src: '/src/js/jquery.min.js', type: 'module' },
+            { src: '/src/js/plugins.js', type: 'module' },
+            { src: '/src/js/scripts.js', type: 'module' },
+          ],
+        },
+      },
+    }),
+  ],
+
   build: {
     outDir: 'dist', // Set the output directory for the production build
   },
   rollupOptions: {
     input: {
       main: './index.html',
-      script1: './src/js/jquery.min.js',
-      script2: './src/js/plugins.js',
-      script3: './src/js/scripts.js',
     },
   },
 })
