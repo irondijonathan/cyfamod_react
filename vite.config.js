d4cb3060bfe62872d5c06f49072a569409999865
Kcee <80968110+irondijonathan@users.noreply.github.com>
2023-07-29 19:02:50 +0000

    build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].js', // Customize the format of the output file names
+      },
+    },
+  },
+
+  // Add an afterBuild hook to delete the specific file after the build
+  // You can adjust the pattern according to the filename that you want to delete
+  afterBuild: () => {
+    const filesToDelete = glob.sync('dist/assets/*.js', {
+      ignore: ['dist/assets/*.css', 'dist/index.html'], // Exclude other files you don't want to delete
+    });
+
+    filesToDelete.forEach((file) => {
+      if (!file.includes('index-')) {
+        fs.unlinkSync(file);
+      }
+    });
+  },
 })
