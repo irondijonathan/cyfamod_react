e0676a171ffd5217523bff08c61c5d246f657d93
Kcee <80968110+irondijonathan@users.noreply.github.com>
2023-07-29 17:02:09 +0000

    build

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ import {Home, Contact, Services, Blog} from "./pages"
 import './App.css'
 import {Routes, Route} from "react-router-dom"
 
-//Js scripts for animations
-import "./../src/js/jquery.min.js"
-import "./../src/js/plugins.js"
-import "./../src/js/scripts.js"
-
-
 
 function App() {
   
